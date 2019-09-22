2d5206bc57a5e22494f5ec5be6bd19f4bbda497d
Krzysztof Skowroński <studio@skovron.pl>
2019-09-22 11:31:30 +0200

    ruch komputera

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let roundNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+if(roundNumber == 1) {
+	computerMove = 'kamień';
+}
+else if(roundNumber == 2) {
+	computerMove = 'papier';
+}
+else if(roundNumber == 3) {
+	computerMove = 'nożyce';
+}
+else {
+	computerMove = 'nienznay ruch';
+}
+
+printMessage('Ruch komputera to: ' + computerMove);
